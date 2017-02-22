d6a956d87f45c7e1fa2458b06ff690522d1bdf55
jessmccay <jessmccay@gmail.com>
2017-02-21 16:03:59 -0800

    WORKING EVENTS on calendar!!!

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,29 +2,18 @@ class WelcomeController < ApplicationController
 
   def index
 
-    @events = Event.all
-
     @concerts = Concert.order 'created_at DESC'
     @books = Book.order 'created_at DESC'
 
   end
 
-  def get_events
-    @events = Event.all
-    events = []
-    @events.each do |event|
-      events << { id: event.id, title: event.title, start: event.start_time}
-    end
-    render :json => events.to_json
-  end
+
 
 
   def show
-    redirect_to 'welcome/calendar'
+    
   end
 
-  def calendar
-    @events = Event.all
-  end
+
 
 end
--- db/migrate/20170221022723_remove_field_name_from_events.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFieldNameFromEvents < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :events, :fieldname, :string
-  end
-end
