d8bc7284a7e47d4ff17b8d5dd4e83696379a3de8
Mrinalini Sinha <ms6471@nyu.edu>
2017-02-24 17:31:31 -0800

    chnaged edit and update forms for events

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,18 +12,24 @@ class WelcomeController < ApplicationController
   end
 
   def search
+
     if !params[:search_params].nil? && !params[:search_params].to_s.strip.empty?
       @concert_results = Concert.search(params[:search_params])
       @book_results = Book.search(params[:search_params])
       @event_results = Event.search(params[:search_params])
       @search_results = @concert_results + @book_results + @event_results
-    elsif !params[:search_params].nil? &&   params[:search_params].to_s.strip.empty?
-      flash.now[:alert] = "You must type something in the search bar."
-      @search_results = nil
+    elsif params[:search_params].nil? || params[:search_params].to_s.strip.empty?
+      redirect_back(fallback_location: root_path) and return
+      flash[:alert] = "You must type something in the search bar."
+    end
+
+    if @search_results.nil? || @search_results.empty?
+      redirect_back(fallback_location: root_path)
+      flash[:alert] = "Sorry! There were no results matching your search."
     end
   end
 
-  
+
   def about
 
   end
