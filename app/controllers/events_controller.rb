acc562834fb64b019a9dff921feee594cb05aed3
cpena002 <carlos_enrique_pena@yahoo.com>
2017-02-26 20:12:43 -0800

    added url field to forms and url to view pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -95,6 +95,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :start_time, :description, :address)
+      params.require(:event).permit(:title, :start_time, :description, :address, :url)
     end
 end
